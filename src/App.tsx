ceb9676de4370e0bcb14397db4b62386d8ddc88f
Jarald Patterson <jeraldpatterson734@gmail.com>
2026-02-27 13:53:30 -0500

    Fix App.tsx corrupted routes

--- src/App.tsx
+++ src/App.tsx
@@ -62,16 +62,6 @@ const App: React.FC = () => {
                 <Route path="/garage-floors" element={<GarageFloors />} />
                 <Route path="/sidewalks" element={<Sidewalks />} />
                 <Route path="/services" element={<Services />} />
-} />
-} />
-} />
-} />
-} />
-                                <Route path="/cedar-hill" element={<Home cityName="Cedar Hill" />} />
-                <Route path="/desoto" element={<Home cityName="DeSoto" />} />
-                <Route path="/duncanville" element={<Home cityName="Duncanville" />} />
-                <Route path="/midlothian" element={<Home cityName="Midlothian" />} />
-                <Route path="/waxahachie" element={<Home cityName="Waxahachie" />} />
                 <Route path="/:city/:service" element={<CityService />} />
                 <Route path="/privacy-policy" element={<PrivacyPolicy />} />
                 <Route path="/terms-of-service" element={<TermsOfService />} />
@@ -87,4 +77,3 @@ const App: React.FC = () => {
 };
 
 export default App;
-
